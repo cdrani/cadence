fcdf8519d92ca95120465c2ff072ff8070040e64
Stephanyy <santosstephany400@gmail.com>
2024-06-11 20:16:01 -0300

    feat: set up environment variables and injecting in manifest

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,17 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import { crx } from '@crxjs/vite-plugin'
 import manifest from './manifest.json'
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+manifest.oauth2.client_id = process.env.SPOTIFY_CLIENT_ID!;
+manifest.oauth2.redirect_uri = `https://${process.env.VITE_EXTENSION_ID}.chromiumapp.org/`;
 
 export default defineConfig({
   plugins: [
     react(),
     crx({ manifest }),
   ],
+
 })
