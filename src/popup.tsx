5d2395a92515269dc2578cf9f8c8cc4173111bfc
cdrani <charlesdrani@gmail.com>
2024-06-18 05:58:05 -0600

    init commit

---
+++ src/popup.tsx
@@ -0,0 +1,16 @@
+function IndexPopup() {
+  return (
+    <div
+      style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            padding: 16,
+            width: 260
+      }}>
+      <h1>Cadence</h1>
+    </div>
+  )
+}
+
+export default IndexPopup
