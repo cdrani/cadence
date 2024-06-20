6c0d0f418b780a616334e09686809bc9944e9e5b
cdrani <charlesdrani@gmail.com>
2024-06-20 00:58:10 -0600

    feat: refresh expired access token on connect

--- src/background/index.ts
+++ src/background/index.ts
@@ -1,2 +1,15 @@
-export {}
-console.log("HELLO WORLD FROM BGSCRIPTS")
+import { Storage } from "@plasmohq/storage"
+
+import type { Session } from "~types/session"
+import { refreshTokens } from "~services/auth"
+
+const storage = new Storage({ area: 'local' })
+
+chrome.runtime.onConnect.addListener(async () => {
+    const session = await storage.get('session') as Session
+
+    if (!session?.expires_at) return
+    if (session.expires_at >= Date.now()) return 
+
+    await refreshTokens(session.refresh_token)
+})
